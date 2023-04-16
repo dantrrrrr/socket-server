5a99049b59f5b83e96dc65b379ea362123ae6503
dantr <huynhngdantr@gmail.com>
2023-04-16 13:06:49 +0700

    cm

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001",
-    methods: ["GET", "POST"],
+    // origin: "http://localhost:3001/",
+    // methods: ["GET", "POST"],
   },
 });
 var Custom = [];
